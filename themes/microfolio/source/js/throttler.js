449ead3148081fc8779ffc66913183c485ac9866
gaggle <mail@jonlauridsen.com>
2016-03-13 17:59:39 +0100

    critical fix

--- themes/microfolio/source/js/throttler.js
+++ themes/microfolio/source/js/throttler.js
@@ -2,13 +2,13 @@
 
 // https://jsfiddle.net/jonathansampson/m7G64/
 module.exports = function (callback, limit) {
-  var wait = false;                  // Initially, we're not waiting
-  return function () {               // We return a throttled function
-    if (!wait) {                   // If we're not waiting
-      callback.call();           // Execute users function
-      wait = true;               // Prevent future invocations
-      setTimeout(function () {   // After a period of time
-        wait = false;          // And allow future invocations
+  var wait = false;                         // Initially, we're not waiting
+  return function () {                      // We return a throttled function
+    if (!wait) {                            // If we're not waiting
+      callback.apply(this, arguments)  // Execute users function
+      wait = true;                          // Prevent future invocations
+      setTimeout(function () {              // After a period of time
+        wait = false;                       // And allow future invocations
       }, limit);
     }
   }
