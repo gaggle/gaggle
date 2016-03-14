a11cafeda18ab7af69bb39c3cce81bfd70ca6c9c
gaggle <mail@jonlauridsen.com>
2016-03-14 03:09:58 +0100

    once a minute change the background to next random slide

--- themes/microfolio/source/js/index.js
+++ themes/microfolio/source/js/index.js
@@ -35,64 +35,55 @@ window.addEventListener("DOMContentLoaded", function (e) {
         "2-4": "Hi, it sure is late, huh?"
       }
     }))
-  var duskData = {
-    name: "dusk",
-    "content": {
-      "background-color": "rgba(20, 20, 20, 0.4)"
-    },
-    "*": {
-      color: "#E6E6E6"
-    }
-  }
-  enrichBackgrounds(stateManager, confParser(
+  enrichBackgrounds(stateManager, [
     {
-      hour: {
-        //"0-24": {
-        //  name: "test",
-        //  content: {"text-shadow": "-3px -3px 1px blue, 3px 3px 2px blue"}
-        //},
-        "5-8": {
-          name: "sunrise",
-          "content": {
-            "background-color": "rgba(20, 20, 20, 0.4)"
-          },
-          "*": {
-            color: "#E6E6E6"
-          }
-        },
-        "9-11": {
-          name: "streaky",
-          "content": {
-            "background-color": "rgba(20, 20, 20, 0.4)"
-          },
-          "*": {
-            color: "#E6E6E6"
-          }
-        },
-        "12-15": {
-          name: "clouds",
-          content: {"text-shadow": "-1px -1px 1px #A69779, 1px 1px 2px #4A7FA8"}
-        },
-        "16-16": {
-          name: "dog",
-          "content": {
-            "background-color": "rgba(50, 50, 50, 0.2)"
-          },
-          "*": {
-            color: "#E6E6E6"
-          }
-        },
-        "17-19": {
-          name: "hospital",
-          "content": {
-            "text-shadow": "0 1px 1px #D9CEB1, 0 -1px 2px #8A8371",
-            "background-color": "rgba(170, 170, 170, 0.3)"
-          }
-        },
-        "20-23": duskData,
-        "0-4": duskData
+      name: "sunrise",
+      "content": {
+        "background-color": "rgba(20, 20, 20, 0.4)"
+      },
+      "*": {
+        color: "#E6E6E6"
       }
-    }))
+    },
+    {
+      name: "streaky",
+      "content": {
+        "background-color": "rgba(20, 20, 20, 0.4)"
+      },
+      "*": {
+        color: "#E6E6E6"
+      }
+    },
+    {
+      name: "clouds",
+      content: {"text-shadow": "-1px -1px 1px #A69779, 1px 1px 2px #4A7FA8"}
+    },
+    {
+      name: "dog",
+      "content": {
+        "background-color": "rgba(50, 50, 50, 0.2)"
+      },
+      "*": {
+        color: "#E6E6E6"
+      }
+    },
+    {
+      name: "hospital",
+      "content": {
+        "text-shadow": "0 1px 1px #D9CEB1, 0 -1px 2px #8A8371",
+        "background-color": "rgba(170, 170, 170, 0.3)"
+      }
+    },
+    {
+      name: "dusk",
+      "content": {
+        "background-color": "rgba(20, 20, 20, 0.4)"
+      },
+      "*": {
+        color: "#E6E6E6"
+      }
+    }
+  ])
   stateController(stateManager)
   mouseTracker(document.getElementsByClassName("content")[0])
   stateManager.start()
