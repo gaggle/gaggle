a582e6b3de6bdb4432d7253edd7e8fc3ca4f1784
Jon Lauridsen <mail@jonlauridsen.com>
2016-04-27 13:59:35 +0100

    look-at module
    simpler than previous mouse-tracker.js module, and tested

---
+++ test/look-at.test.js
@@ -0,0 +1,41 @@
+"use strict";
+var tracker = require("../themes/microfolio/source/js/look-at")
+
+describe("look-at", function () {
+  var center, el
+  before(function () {
+    center = {x: document.documentElement.clientWidth / 2, y: document.documentElement.clientHeight / 2}
+  })
+
+  beforeEach(function () {
+    el = document.createElement("div")
+  })
+
+  it("should have zero rotation in middle of screen", function () {
+    tracker(el, center.x, center.y)
+    expect(el.style.transform).to.include("rotateY(0deg) rotateX(0deg)")
+  })
+
+  it("should limit rotation", function () {
+    tracker(el, 0, center.y)
+    expect(el.style.transform).to.include("rotateY(-89deg)")
+    tracker(el, 0, center.y, {max_degrees: 3})
+    expect(el.style.transform).to.include("rotateY(-3deg)")
+  })
+
+  it("should always set perspective", function () {
+    tracker(el, center.x, center.y)
+    expect(el.style.transform).to.include("perspective")
+  })
+
+  it("should support perspective from options", function () {
+    tracker(el, center.x, center.y, {perspective: "1000px"})
+    expect(el.style.transform).to.include("perspective(1000px)")
+  })
+
+  it("should grab perspective from element's style", function () {
+    el.style.perspective = "1000px"
+    tracker(el, center.x, center.y)
+    expect(el.style.transform).to.include("perspective(1000px)")
+  })
+})
---
+++ themes/microfolio/source/js/look-at.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var lookAt = function (el, x, y, opts) {
+  opts = parseOpts(opts, el)
+  var raw_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+  var w = raw_width / 2
+  var raw_height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+  var h = raw_height / 2
+
+  var factor_x = opts.max_degrees / w
+  var factor_y = opts.max_degrees / h * -1
+
+  var degX = (x - w) * factor_x
+  var degY = (y - h) * factor_y
+
+  el.style.transform = "perspective(" + opts.perspective + ") " +
+    "rotateY(" + degX + "deg) " +
+    "rotateX(" + degY + "deg)"
+}
+
+var parseOpts = function (raw_opts, el) {
+  var opts = raw_opts || {}
+  if (!opts.max_degrees) opts.max_degrees = 89
+  if (!opts.perspective) opts.perspective = getComputedStyle(el).perspective || el.style.perspective || "500px"
+  return opts
+}
+
+module.exports = lookAt
