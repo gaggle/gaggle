3227438089865b62bedb652a3cccefd415aa0fb6
Jon Lauridsen <mail@jonlauridsen.com>
2016-05-15 16:45:33 +0100

    Pull custom browsers to separate module

--- karma.ci.conf.js
+++ karma.ci.conf.js
@@ -1,36 +1,5 @@
 module.exports = function (config) {
-  var customLaunchers = {
-    sl_chrome: {
-      base: "SauceLabs",
-      browserName: "chrome",
-      platform: "Windows 10",
-      version: "latest"
-    },
-    sl_edge: {
-      base: "SauceLabs",
-      browserName: "MicrosoftEdge",
-      platform: "Windows 10",
-      version: "latest"
-    },
-    sl_ff: {
-      base: "SauceLabs",
-      browserName: "firefox",
-      platform: "Linux",
-      version: "latest"
-    },
-    sl_ie: {
-      base: "SauceLabs",
-      browserName: "internet explorer",
-      platform: "Windows 10",
-      version: "latest"
-    },
-    sl_safari: {
-      base: "SauceLabs",
-      browserName: "safari",
-      platform: "OS X 10.11",
-      version: "latest"
-    }
-  }
+  var customLaunchers = require("./sauce-labs-browsers")()
 
   config.set({
     basePath: "",
