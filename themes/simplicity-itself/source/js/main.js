ffbfb544a2524918f85df195e5b02568c09a4026
Jon Lauridsen <mail@jonlauridsen.com>
2016-12-03 13:11:02 +0000

    Unobtrusive .js for .relative-time

--- themes/simplicity-itself/source/js/main.js
+++ themes/simplicity-itself/source/js/main.js
@@ -1,3 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
+var moment = require("moment")
+
+var update_datetime = function (el) {
+  var d = el.getAttribute("data-datetime")
+  el.textContent = moment(d).fromNow()
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".relative-time").forEach(update_datetime)
   console.log("Ready")
 })
