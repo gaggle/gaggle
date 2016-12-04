d52fa33d73070f292cea7a8cf73f3a35224fb290
Jon Lauridsen <mail@jonlauridsen.com>
2016-12-04 22:19:33 +0000

    Enable SauceLabs

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,29 @@
+let browsers = ["PhantomJS"]
+let customLaunchers = undefined
+let reporters = ["dots", "coverage"]
+let concurrency = Infinity
+
+const SECOND = 1000
+const MINUTE = 60 * SECOND
+
+if (process.env.SAUCE_USERNAME) {
+  const slBrowsers = require("./saucelabs-browsers")
+
+  customLaunchers = slBrowsers({
+    "name-prefix": "frontend-unit-test",
+    tunnel: process.env.TRAVIS_JOB_NUMBER,
+    build: process.env.TRAVIS_BUILD_NUMBER
+  })
+  browsers = Object.keys(customLaunchers)
+  reporters.push("saucelabs")
+  concurrency = 5
+}
+
 module.exports = function (config) {
   config.set({
     basePath: "",
-    browsers: ["PhantomJS"],
+    customLaunchers: customLaunchers,
+    browsers: browsers,
     frameworks: ["mocha", "sinon-chai", "browserify"],
     files: [
       "test/frontend/**/*.js",
@@ -11,7 +33,7 @@ module.exports = function (config) {
       "test/**/*.js": ["browserify"],
       "themes/**/*.js": ["browserify"]
     },
-    reporters: ["dots", "coverage"],
+    reporters: reporters,
     coverageReporter: {
       reporters: [
         {"type": "text"},
@@ -23,12 +45,24 @@ module.exports = function (config) {
     logLevel: config.LOG_ERROR,
     autoWatch: false,
     singleRun: true,
-    concurrency: Infinity,
+    concurrency: concurrency,
+
+    // Keept getting "Disconnected, because no message in 10000 ms." from Firefox
+    browserDisconnectTimeout: 10 * SECOND, // default 2000
+    browserDisconnectTolerance: 1,
+    browserNoActivityTimeout: MINUTE,
+    captureTimeout: 2 * MINUTE,
+
+    // Extensions
     browserify: {
       debug: true,
       transform: [
         ["browserify-istanbul", {instrumenterConfig: {embedSource: true}}]
       ]
+    },
+    sauceLabs: {
+      startConnect: false,
+      tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER
     }
   })
 }
--- test/karma.conf.test.js
+++ test/karma.conf.test.js
@@ -47,5 +47,36 @@ describe("karma.conf", function () {
       expect(conf.reporters).to.include("coverage")
     })
   })
+
+  describe("with SauceLabs active", () => {
+    beforeEach(() => {
+      process.env.SAUCE_USERNAME = "sauce_user"
+      process.env.TRAVIS_JOB_NUMBER = "123"
+      process.env.TRAVIS_BUILD_NUMBER = "abc"
+      conf = get_conf()
+    })
+
+    it("should specify many browsers", () => {
+      expect(conf.browsers.length).to.be.above(1)
+    })
+
+    it("should specify many custom launchers", () => {
+      expect(Object.keys(conf.customLaunchers).length).to.be.above(1)
+    })
+
+    it("should pass Travis job number as tunnel identifier", () => {
+      const first_browser = conf.customLaunchers[conf.browsers[0]]
+      expect(first_browser["tunnel-identifier"]).to.eq("123")
+    })
+
+    it("should pass Travis build number as build", () => {
+      const first_browser = conf.customLaunchers[conf.browsers[0]]
+      expect(first_browser.build).to.eq("abc")
+    })
+
+    it("should specify saucelabs as reporter", () => {
+      expect(conf.reporters).to.include("saucelabs")
+    })
+  })
 })
 
