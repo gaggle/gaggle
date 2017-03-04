edd09864be485b1ace7abc8bc76b27f741d09a93
Jon Lauridsen <mail@jonlauridsen.com>
2017-03-04 10:06:34 +0000

    Trivial

--- test/ui/blog-entry/alpha-post.test.js
+++ test/ui/blog-entry/alpha-post.test.js
@@ -6,7 +6,7 @@ describe("blog entry", () => {
   for (let e of uiTesting.sizes) {
     (function (size) {
       let size_str = `${size.width}x${size.height}`
-      it(`renders an alpha post (${size_str})`, () => {
+      it(`shows an alpha post (${size_str})`, () => {
         BlogEntryPage.open("1980/alpha/")
         BlogEntryPage.size(size)
       })
