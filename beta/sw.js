d055b5145b2d5353cfda94b1f6c9cab02e9e9e46
Travis CI <travis@travis-ci.org>
2019-02-03 22:46:27 +0000

    Deploy beta to gh-pages: 87f106c98edfe158f323892fa1c454f6f640dd64

--- beta/sw.js
+++ beta/sw.js
@@ -2,24 +2,24 @@ importScripts('/beta/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/beta/_nuxt/60d07c64e2a55743f1bf.js",
-    "revision": "73d8c452a4a12efcfc3a3333ac5a509c"
+    "url": "/beta/_nuxt/12c89704e0615ca6f848.js",
+    "revision": "984d161839e0dd214eff5ca53987f97e"
   },
   {
-    "url": "/beta/_nuxt/699f21fa71b8b2af24c6.js",
-    "revision": "0348e8e74a23682fe80aca4517c2d3fb"
+    "url": "/beta/_nuxt/60d07c64e2a55743f1bf.js",
+    "revision": "73d8c452a4a12efcfc3a3333ac5a509c"
   },
   {
     "url": "/beta/_nuxt/6d290124443db53d0bb8.js",
     "revision": "968461f5f8824c2368868a39c34cf70b"
   },
   {
-    "url": "/beta/_nuxt/857fed9df54b5242648c.js",
-    "revision": "556081565046bec7ac1f0bb33332dd48"
-  },
-  {
     "url": "/beta/_nuxt/8a20c176ff4f8b97637a.js",
     "revision": "02ba0f9c4c5f2c23aa71579937a3fe81"
+  },
+  {
+    "url": "/beta/_nuxt/f931e9735a9db69a653d.js",
+    "revision": "e0149bf34c47297bbb7a56549f567644"
   }
 ], {
   "cacheId": "gaggle",
