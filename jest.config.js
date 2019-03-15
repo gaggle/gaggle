4237b3a2931b71b4cc7980ad6081b091ec38f8ea
Jon Lauridsen <mail@jonlauridsen.com>
2019-03-15 02:55:27 +0100

    Add test setup file to raise console warn+error as exceptions

--- jest.config.js
+++ jest.config.js
@@ -7,9 +7,10 @@ module.exports = {
   moduleFileExtensions: ['js', 'vue', 'json'],
   transform: {
     '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest'
+    '^.+\\.vue$': 'vue-jest'
   },
   collectCoverage: true,
-  collectCoverageFrom: ['**/*.{js,vue}', '!{.}*', '!coverage/**/*'],
-  coverageReporters: ['text', 'html', 'lcov']
+  collectCoverageFrom: ['**/*.{js,vue}', '!*', '!coverage/**/*', '!static/sw.js'],
+  coverageReporters: ['text', 'html', 'lcov'],
+  setupFiles: ['./test/jest.overrides.js']
 }
