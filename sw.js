b447b9ce507f39884c5db910b31ac4df677b4d89
Jon Lauridsen <mail@jonlauridsen.com>
2022-12-31 13:50:43 +0100

    Deploy build to gh-pages: 92a5754169f7e0a35ba2da7e4d294724fddaa182

--- sw.js
+++ sw.js
@@ -10,16 +10,16 @@ workbox.precaching.precacheAndRoute([
     "revision": "1b16799752b948186dc4b75675bc8152"
   },
   {
-    "url": "/_nuxt/4f05dcc.js",
-    "revision": "fc7edd0c60c822acc588c3331d0d8193"
-  },
-  {
     "url": "/_nuxt/6e9e525.js",
     "revision": "d6d4ea9a1740cec715d16d2b911f9c86"
   },
   {
-    "url": "/_nuxt/e4863cc.js",
-    "revision": "9f66a48c1df3b3c3148fa1def9f1519b"
+    "url": "/_nuxt/ad1e3c8.js",
+    "revision": "807f50c57cd470f65113afd9f245a1d4"
+  },
+  {
+    "url": "/_nuxt/af4b488.js",
+    "revision": "ed95f1ebb284ffec00160d3e965d71a8"
   }
 ], {
   "cacheId": "gaggle",
